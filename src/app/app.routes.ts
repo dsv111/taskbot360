3dfe5b71ca53c9862546478f7ca1e00ae8e7ff74
dsv111 <srinivas.dasari2019@gmail.com>
2025-08-24 07:50:37 +0530

    test1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from '../pages/home/home.component';
 import { ChatComponent } from '../pages/chat/chat.component';
 
 export const routes: Routes = [
-    {path:'home',component:HomeComponent},
+    {path:'',component:HomeComponent},
     {path:'chat',component:ChatComponent},
     { path: '**', redirectTo: '' }
 
